91f588dd0706e9eb9c1d77e65cd41aeb832d6874
praveen <praveen.mut@gmail.com>
2019-07-07 18:46:21 +1000

    set up initial states

--- src/index.js
+++ src/index.js
@@ -3,13 +3,34 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Latitude</div>
+      (position) => {
+        this.setState({ lat: position.coords.latitude })
+      },
+      (err) => { 
+        this.setState({ errorMessage: err.message }) 
+    });
   }
+  render() {
+    if(this.state.errorMessage && !this.state.lat) {
+      return (
+      <div>Error: {this.state.errorMessage}</div>
+      );
+    };
+    if(!this.state.errorMessage && this.state.lat) {
+      return (
+      <div>Latitude: {this.state.lat}</div>
+      );
+    };
+    return (
+    <div>Loading!</div>
+    );
+  };
 }
 
 ReactDOM.render(
